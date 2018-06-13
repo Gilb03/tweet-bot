3c4b6c1dd719d5a9dd339f317866726d7af7ae31
Gilbert King <gilbertking91@gmail.com>
2018-06-13 11:14:03 -0400

    third commit

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const Twit = require('Twit'),
-      T    = newTwit({
-            consumer key: '. . .',
-            consumer_secret: ' . . ',
-            access_token:'. . .',
+const Twit = require('twit')
+const  T = new Twit(require('./server.js'))
 
+/* const stream = T.stream('statuses/filter', {follow: users})  
+stream.on('tweet', function (tweet){
+    if(users.indexOf(tweet.user.id_str) > -1){
+        console.log(tweet.user.name + ':' + tweet.text)
+        T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response){
+            console.log(data)
         })
+   }
+})*/
+const User = [79543151835113089]
+const stream = T.stream('statuses/filter', {follow: User})
+stream.on('tweet', function (tweet){
+    if(users.indexOf(tweet.user.id_str) > -1){
+        console.log(tweet.user.name + ':' + tweet.text)
+    } T.post('statusesretweet/:id', { id: '79543151835113089'}, function(err, data, response){
+        console.log(data)
+})
+
+retweetLatest()
+setInterval(retweetLatest, 100 * 60 * 12)})
+
+// make sure it worked
+function retweeted (err, reply){
+    if (err !== undefined){
+        console.log(err)
+    } else {
+        console.log('its good')
+    }
+}
