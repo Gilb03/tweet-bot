d37728f4a05e3aacf32959461b91d9cdd34f6b21
Gilbert King <gilbertking91@gmail.com>
2018-07-19 23:06:33 -0400

    stream functionality

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
     // this is gthebrogod twitter
       module.exports = {
-            consumer_key: '. . . '
-            , consumer_secret: '. . . '
-            , access_token:'. . . '
-            , access_token_secret:'. . . '
+            consumer_key: 'iXOEonsqMoRMQH5BKFSk78P13'
+            , consumer_secret: 'dOuw9H7sIhj8PwACVGlTyTRgEZfLKqnOqpjK3rJ6KdEWtlze5x'
+            , access_token: '222901626-qjjZHXuQg5iLMokh2rQjghFHzFuylrcHndw2Wti4'
+            , access_token_secret:'t6eS9ajV7uLWt2BSWGyKcFQmttL86ngLgrvqmzgtR1YnW'
             , timeout_ms: 60*1000,
               strictSSL:  true,
               app_only_auth: true,
